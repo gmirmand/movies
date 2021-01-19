9cae41540cc952c1538ee01a8a3af728468a8fe9
gmirmand <gmirmand@gmail.com>
2021-01-19 22:08:15 +0100

    Global improve

--- js/plugins.js
+++ js/plugins.js
@@ -26,7 +26,7 @@ var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 var cH;
 var cW;
-var bgColor = "#FF6138";
+var bgColor = "#282741";
 var animations = [];
 var circles = [];
 
